acd7b2f91cc75142e994db2f3595bc1d844c8866
Ian Cullum <ian.p.cullum@gmail.com>
2021-11-20 18:49:23 -0800

    Steven - Main menu Aesthetics and Controls
    Also added 2 scripts for making arrow keys work and such

---
+++ The After-Party/Assets/Scripts/LevelSelect.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelect : MonoBehaviour
+{
+    public void Level1() {  
+        SceneManager.LoadScene("test");  
+    }
+
+    public void Level2() {  
+        SceneManager.LoadScene("test");  
+    }
+
+    public void Level3() {  
+        SceneManager.LoadScene("test");  
+    }
+
+    public void LevelTest() {  
+        SceneManager.LoadScene("test");  
+    }
+}
